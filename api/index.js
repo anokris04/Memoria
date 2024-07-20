3d2b03aad827ff5bade7431d94d43f0dd2bec88f
Anoop Rastogi <krishnaanoop03.ak@gmail.com>
2024-07-20 20:44:19 +0530

    api profile page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from 'cors';
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ const app = express();
 app.use(cors());
 //middle ware allowing to use json data
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
